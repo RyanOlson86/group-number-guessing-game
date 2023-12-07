c22e9b66d28fe7bdd7d8f8f1dacc657519366c94
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-07 11:17:09 -0600

    Set server.js and added get and post functions

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const PORT = 5000;
-
+const PORT = 5001;
+let results =[]
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(bodyParser.json())
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+app.get("/guess", (req, res) => {
+console.log("in get /guess", results);
+res.send(results)
+})
+
+app.post("/guess", (req, res) => {
+console.log("in POST /guess", req.body);
+results.push(req.body)
+console.log("checking results:", results);
+res.sendStatus(201)
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
